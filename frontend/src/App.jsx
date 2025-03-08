44e3186d3dae911cd5ead048c8018197c32ee33b
mojtaba-bb <10mojijom01@gmail.com>
2025-03-08 23:56:32 +0330

    starter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { Camera } from 'lucide-react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <body>
-
+        <Camera />
     </body>
   )
 }
